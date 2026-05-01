ad89213161c5d9eae289702b51abce4a7ca9acbb
Liano <liano.caekebeke@student.ucll.be>
2026-05-01 11:07:19 +0200

    fix lowercase image name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello CI/C!');
+  res.end('Hello CI/Ci!');
 });
 
 server.listen(3000, () => console.log('Server running on port 3000'));
