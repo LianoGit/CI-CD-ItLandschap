2dc9e183a8cf62ec3a87a22493718653a3f13d76
Liano <liano.caekebeke@student.ucll.be>
2026-05-01 11:08:48 +0200

    testing2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello CI/Ci!!');
+  res.end('Hello CI/CD!');
 });
 
 server.listen(3000, () => console.log('Server running on port 3000'));
